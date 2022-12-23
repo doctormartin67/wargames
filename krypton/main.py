9603cd46883c670b717786268151a6193f8ef13f
doctormartin67 <celticfans@hotmail.com>
2022-12-23 17:13:14 +0100

    krypton5

--- krypton/main.py
+++ krypton/main.py
@@ -1,60 +1,76 @@
 import sys
 
-GROUPS = 1
-key = "FREKEY"
-letter = 5
-
-"""
-if we assume that "E" is the most common letter, then "J" corresponds to "F"
-for the first letter of the key. I will continue this process:
-J->F
-V->R
-I->E
-O->K
-I->E
-C->Y
-
-"""
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+assert(len(alpha) == 26)
 
 def rotate(s, n):
     return s[n:] + s[:n]
 
-def create_group(s, n):
+def create_group(s, key_len, letter):
     group = []
     i = 0
     for a in s:
         if i < letter:
             i += 1
             continue
-        if (i - letter) % len(key):
+        if (i - letter) % key_len:
             i += 1
             continue # only looking at first letter of key
-        if not i % n:
-            group += a
-        else:
-            group[len(group)-1] += a
+        group += a
         i += 1
 
     return group
 
-f = open(sys.argv[1])
-s = ""
-for l in f:
-    s += l
+def decrypt(file_name, key):
+    f = open(file_name)
+    s = ""
+    for l in f:
+        s += l
 
-s = s.replace(" ", "")
+    s = s.replace(" ", "")
+    i = 0
+    for a in s:
+        print(alpha[(alpha.find(a) - alpha.find(key[i])) % 26], end="")
 
-group = []
+        i += 1
+        i %= len(key)
 
-for i in range(0, GROUPS):
-    tmp = rotate(s, i)
-    group += create_group(tmp, GROUPS)
+def letters(file_name, key_len, letter):
+    f = open(file_name)
+    s = ""
+    for l in f:
+        s += l
 
-count = {i: group.count(i) for i in group}
-if 1:
-    for key in count.keys():
-        print(key, count[key])
+    s = s.replace(" ", "")
+    group = create_group(s, key_len, letter)
+    count = {i: group.count(i) for i in group}
+    return count
 
-if 0:
-    print(s)
+def addtod(d1, d2):
+    for k in d1.keys():
+        if k in d2.keys():
+            d1[k] += d2[k]
+
+    for k in d2.keys():
+        if k not in d1.keys():
+            d1[k] = d2[k]
+
+if __name__ == "__main__":
+    key_len = int(sys.argv[1])
+
+    key = ""
+    for i in range(0, key_len):
+        l1 = letters("found1", key_len, i)
+        l2 = letters("found2", key_len, i)
+        l3 = letters("found3", key_len, i)
+
+        d = {}
+        addtod(d, l1)
+        addtod(d, l2)
+        addtod(d, l3)
+
+        key += alpha[(ord(max(d, key=d.get)) - ord("E")) % 26]
+
+    print(key)
+    decrypt("krypton6", key)
 
