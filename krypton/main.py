2e2220332143b8fc424a2263f63838932bd11104
doctormartin67 <celticfans@hotmail.com>
2022-12-23 15:55:14 +0100

    krypton4

--- krypton/main.py
+++ krypton/main.py
@@ -1,6 +1,20 @@
 import sys
 
-GROUPS = 4
+GROUPS = 1
+key = "FREKEY"
+letter = 5
+
+"""
+if we assume that "E" is the most common letter, then "J" corresponds to "F"
+for the first letter of the key. I will continue this process:
+J->F
+V->R
+I->E
+O->K
+I->E
+C->Y
+
+"""
 
 def rotate(s, n):
     return s[n:] + s[:n]
@@ -9,13 +23,18 @@ def create_group(s, n):
     group = []
     i = 0
     for a in s:
+        if i < letter:
+            i += 1
+            continue
+        if (i - letter) % len(key):
+            i += 1
+            continue # only looking at first letter of key
         if not i % n:
             group += a
         else:
             group[len(group)-1] += a
         i += 1
 
-    count = {i: group.count(i) for i in group}
     return group
 
 f = open(sys.argv[1])
@@ -25,44 +44,17 @@ for l in f:
 
 s = s.replace(" ", "")
 
-s = s.replace("J", "t")
-s = s.replace("D", "h")
-s = s.replace("S", "e")
-
-s = s.replace("Q", "a")
-#s = s.replace("F", "v")
-
-s = s.replace("G", "n")
-s = s.replace("W", "d")
-
-s = s.replace("C", "i")
-s = s.replace("T", "m")
-
-s = s.replace("U", "s")
-s = s.replace("A", "b")
-s = s.replace("X", "f")
-s = s.replace("O", "x")
-
-"""
-used krypton4 for the following replaces guesses:
-"""
-
-s = s.replace("Y", "p")
-s = s.replace("K", "w")
-s = s.replace("B", "o")
-s = s.replace("N", "r")
-s = s.replace("V", "l")
-s = s.replace("M", "u")
-
-print(s)
-sys.exit(0)
-
 group = []
 
 for i in range(0, GROUPS):
     tmp = rotate(s, i)
     group += create_group(tmp, GROUPS)
+
 count = {i: group.count(i) for i in group}
-for key in count.keys():
-    print(key, count[key])
+if 1:
+    for key in count.keys():
+        print(key, count[key])
+
+if 0:
+    print(s)
 
